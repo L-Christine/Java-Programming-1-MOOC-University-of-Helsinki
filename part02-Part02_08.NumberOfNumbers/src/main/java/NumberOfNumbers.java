903a678880bd25c9b098f5a53c73bee124681346
Christine Lee <59518411+L-Christine@users.noreply.github.com>
2023-08-24 02:17:02 -0400

    Add files via upload

---
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class NumberOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int count = Integer.valueOf(scanner.nextLine());
+
+            if (count == 0) {
+                break;
+            }
+
+            if (count > 0 || count < 0) {
+                number = number + 1;
+            }
+
+        }
+        System.out.println("Number of numbers: " + number);
+
+    }
+}
