fef80022cb4480fb22646ee00fdf30e25658b153
Christine Lee <59518411+L-Christine@users.noreply.github.com>
2023-09-18 00:57:27 -0400

    Add files via upload

---
+++ part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
@@ -0,0 +1,69 @@
+
+public class SimpleDate {
+
+    private int day;
+    private int month;
+    private int year;
+
+    public SimpleDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public void advance() {
+        this.day += 1;
+        
+        if (this.day > 30) {
+            this.month = this.month + 1;
+            this.day = 1;
+            
+            if (this.month > 12) {
+                this.year += 1;
+                this.month = 1;
+            }
+        }
+    }
+
+    public void advance(int howManyDays) {
+        int count = 0;
+        while (count < howManyDays) {
+            this.advance();
+            count += 1;
+        }
+    }
+
+    public SimpleDate afterNumberOfDays(int days) {
+        SimpleDate newDate = new SimpleDate(this.day, this.month, this.year);
+        
+        int count = 0;
+        while (count < days) {
+            newDate.advance();
+            count += 1;
+        }
+        return newDate;
+    }
+
+    @Override
+    public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
+
+    public boolean before(SimpleDate compared) {
+        if (this.year < compared.year) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month < compared.month) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month == compared.month
+                && this.day < compared.day) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
