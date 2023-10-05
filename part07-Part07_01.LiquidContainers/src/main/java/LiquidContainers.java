a406363d9141509f80c8c9277d202767f9f8f7fd
Christine Lee <59518411+L-Christine@users.noreply.github.com>
2023-10-05 07:40:36 -0400

    Add files via upload

---
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -0,0 +1,61 @@
+
+import java.util.Scanner;
+
+public class LiquidContainers {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int first = 0;
+        int second = 0;
+
+        while (true) {
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
+
+            String input = scan.nextLine();
+            if (input.equals("quit")) {
+                break;
+            }
+
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+
+            if (command.equals("add") && amount >= 0) {
+                if (first + amount <= 100) {
+                    first += amount;
+                } else if (first + amount > 100) {
+                    first = 100;
+                }
+            }
+
+            if (command.equals("move") && amount >= 0) {
+                if (first - amount >= 0) {
+                    second += amount;
+                    first -= amount;
+                    if (second >= 100) {
+                        second = 100;
+                    }
+                } else if (first - amount < 0) {
+                    second += first;
+                    first = 0;
+                    if (second >= 100) {
+                        second = 100;
+                    }
+                }
+            }
+
+            if (command.equals("remove") && amount >= 0) {
+                if (second - amount >= 0) {
+                    second -= amount;
+                } else if (second - amount < 0) {
+                    second = 0;
+                }
+
+            }
+        }
+
+    }
+
+}
